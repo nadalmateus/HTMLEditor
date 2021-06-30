e8694125e93ed6b4fce8de7958452607ca73c6b9
Mateus Nadal <nadalmateus@hotmail.com>
2021-06-30 20:44:04 -0300

    DrawnMenu

--- Menu.cs
+++ Menu.cs
@@ -8,16 +8,21 @@ namespace HTMLEditor
         {
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ForegroundColor = ConsoleColor.White;
-
-
-
-
-
+            Console.ForegroundColor = ConsoleColor.Black;
+            DrawnScreen();
         }
 
-        public static void Hide()
+        public static void DrawnScreen()
         {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
+
 
         }
     }
